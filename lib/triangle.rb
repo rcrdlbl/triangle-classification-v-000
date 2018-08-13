47c717042aece0b81d9079fd51787b9c2f578964
Przemyslaw Dabrowski <bluh@outlook.com>
2018-08-13 18:16:09 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -3,14 +3,15 @@ class Triangle
   attr_accessor :side1, :side2, :side3
 
   def initialize(side1:, side2:, side3:)
-    @side1 = side1
-    @side2 = side2
-    @side3 = side3
+    
   end
 
   def kind
   end
 
   class TriangleError < StandardError
+    def message
+      "not triangle"
+    end
   end
 end
