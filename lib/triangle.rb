3590649f7bf983f8e879cf6a5320ae207ec08efa
Przemyslaw Dabrowski <bluh@outlook.com>
2018-08-13 18:32:23 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,7 +2,7 @@ class Triangle
   # write code here
   attr_accessor :side1, :side2, :side3
 
-  def initialize(side1:, side2:, side3:)
+  def initialize(side1, side2, side3)
     @side1, @side2, @side3 = [side1, side2, side3].sort
     raise TriangleError if @side1 <= 0 or @side1 + @side2 <= @side3
   end
